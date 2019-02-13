fefd8f56663df600891e8b0ebb21771a4b36ed30
yuzp1996 <yuzp1996@qq.com>
2019-02-13 18:49:39 +0800

    good

--- golang/array/array.go
+++ golang/array/array.go
@@ -25,7 +25,7 @@ func (this *Array) Len() int {
 }
 
 func (this *Array) isIndexOutOfRange(index int) bool {
-	if index <= 0 || index >= cap(this.data) {
+	if index <= 0 || index > cap(this.data) {
 		return true
 	}
 	return false
@@ -52,7 +52,6 @@ func (this *Array) Insert(index, value int)(error){
 		return nil
 	}
 	for i:=this.Len();i>=index;i--{
-
 		this.data[i] = this.data[i-1]
 	}
 	this.data[index-1] = value
@@ -74,25 +73,57 @@ func (this *Array) Delete(index int)(int, error){
 }
 
 
+
 func main(){
     newArray := NewArray(6)
-    for i:=1;i<=5;i++{
-    	 if err:= newArray.Insert(i,1); err != nil{
+    for i:=1;i<=6;i++{
+    	 if err:= newArray.Insert(i,i); err != nil{
     	 	 fmt.Println("err is ", err)
 			 return
 		 }
 	}
+    bignewarray := NewArray(6)
+    fmt.Println(bignewarray)
+	for i:=1;i<=6;i++{
+		if err:= bignewarray.Insert(i,i+2); err != nil{
+			fmt.Println("err is ", err)
+			return
+		}
+	}
     fmt.Println(newArray)
-    _ = newArray.Insert(5,3)
-    _ = newArray.Insert(4,4)
-    //_ = newArray.Insert(2,3)
-	fmt.Println(newArray)
-	newArray.Delete(2)
-    newArray.Delete(3)
-	fmt.Println(newArray)
-    val,_ := newArray.Find(3)
-    fmt.Println(val)
+	fmt.Println(bignewarray)
+	fianalArray := NewArray(20)
+
+	if newArray.Len()==0||bignewarray.Len()==0{
+		fianalArray.data = append(newArray.data,bignewarray.data...)
+		fmt.Println(fianalArray)
+	}
+	for{
+		newindex := 0
+		bigindex := 0
+		if newArray.Len()>0 && bignewarray.Len()>0{
+			if newArray.data[newindex]>=bignewarray.data[bigindex]{
+				_ = fianalArray.Insert(fianalArray.length,newArray.data[newindex])
+				newindex++
+				_,_ = newArray.Delete(0)
+			}else{
+				_ = bignewarray.Insert(fianalArray.length,bignewarray.data[bigindex])
+				bigindex++
+				_,_ = bignewarray.Delete(0)
+			}
+
+		}else{
+			if newArray.Len()==0{
+				fianalArray.data = append(fianalArray.data,newArray.data...)
+			}else{
+				fianalArray.data = append(fianalArray.data,bignewarray.data...)
+			}
+			break
+		}
+	}
+	fmt.Println(fianalArray)
 }
+
 
 
 
@@ -101,6 +132,7 @@ func main(){
 //所以0的输入应该是不对的 用户不应该有0的输入 最小是1
 //最好是用数据手写一下 试试看
 //如果insert加一了  那就应该进行一次length++
+//自己手写 也是很重要的
 
 
 
