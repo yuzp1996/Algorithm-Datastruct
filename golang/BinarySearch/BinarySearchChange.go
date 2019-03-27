35cb62f9a9524dac887d6ee9409417e9cb90b7ac
yuzp1996 <yuzp1996@qq.com>
2019-03-28 07:49:11 +0800

    binary search hcange

--- golang/BinarySearch/BinarySearchChange.go
+++ golang/BinarySearch/BinarySearchChange.go
@@ -45,3 +45,39 @@ func Getlastindex(array []int, target int)int{
 	}
 	return -1
 }
+
+//第一个大于等于给定值  往左找
+func GetFirstBigger(array []int, target int)int{
+	per := 0
+	last := len(array)-1
+	for per<=last{
+		mid := (per+last)/2
+		if array[mid]>=target{
+			if mid==0||array[mid-1]<target{
+				return mid
+			}
+			last = mid-1
+		}else if array[mid]<target {
+			per = mid + 1
+		}
+	}
+	return -1
+}
+
+//最后一个小于等于给定值 往右找
+func Lastsamller(array []int,target int)int{
+	per := 0
+	last := len(array)-1
+	for per<=last{
+		mid := (per+last)/2
+		if array[mid]<=target{
+			if mid == len(array)-1||array[mid+1]>target{
+				return mid
+			}
+			per = mid+1
+		}else if array[mid]>target{
+			last = mid-1
+		}
+	}
+	return -1
+}
