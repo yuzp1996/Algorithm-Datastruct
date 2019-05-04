8bddfd17c05d5c77ff65f55f693ffff5a7ec7ec2
yuzp1996 <yuzp1996@qq.com>
2019-05-04 13:20:30 +0800

    before insert

--- golang/skiplist/skiplist.go
+++ golang/skiplist/skiplist.go
@@ -1,124 +1,40 @@
 package main
 
 import (
-	"fmt"
 	"math/rand"
 	"time"
 )
 
-const MaxLevel = 32
-const Probability = 0.25 // 基于时间与空间综合 best practice 值, 越上层概率越小
+const MaxLevel  = 7
 
-func randLevel() (level int) {
+const Probability = 0.25
+
+func randlevel(level int){
 	rand.Seed(time.Now().UnixNano())
-	for level = 1; rand.Float32() < Probability && level < MaxLevel; level++ {
-		// fmt.Println(rand.Float32())
+	for level = 1; rand.Float32()<Probability && level <= MaxLevel; level++{
+
 	}
-	// fmt.Printf("up to %d level\n", level)
 	return
 }
 
+
+
 type node struct {
+	key int
 	forward []*node
-	key     int
 }
 
-type skipList struct {
-	head  *node
+type skiplist struct {
+	head *node
 	level int
 }
 
-func newNode(key, level int) *node {
-	return &node{key: key, forward: make([]*node, level)}
+func NewNode(key, level int)*node{
+	return &node{key:key,forward:make([]*node, level)}
 }
 
-func newSkipList() *skipList {
-	return &skipList{head: newNode(0, MaxLevel), level: 1}
+func NewSkipList()*skiplist{
+	return &skiplist{head:NewNode(0, MaxLevel),level:1}
 }
 
-func (s *skipList) insert(key int) {
-	current := s.head
-	update := make([]*node, MaxLevel) // 新节点插入以后的前驱节点
-	for i := s.level - 1; i >= 0; i-- {
-		if current.forward[i] == nil || current.forward[i].key > key {
-			update[i] = current
-		} else {
-			for current.forward[i] != nil && current.forward[i].key < key {
-				current = current.forward[i] // 指针往前推进
-			}
-			update[i] = current
-		}
-	}
 
-	level := randLevel()
-	if level > s.level {
-		// 新节点层数大于跳表当前层数时候, 现有层数 + 1 的 head 指向新节点
-		for i := s.level; i < level; i++ {
-			update[i] = s.head
-		}
-		s.level = level
-	}
-	node := newNode(key, level)
-	for i := 0; i < level; i++ {
-		node.forward[i] = update[i].forward[i]
-		update[i].forward[i] = node
-	}
-}
-
-func (s *skipList) delete(key int) {
-	current := s.head
-	for i := s.level - 1; i >= 0; i-- {
-		for current.forward[i] != nil {
-			if current.forward[i].key == key {
-				tmp := current.forward[i]
-				current.forward[i] = tmp.forward[i]
-				tmp.forward[i] = nil
-			} else if current.forward[i].key > key {
-				break
-			} else {
-				current = current.forward[i]
-			}
-		}
-	}
-
-}
-
-func (s *skipList) search(key int) *node {
-	// 类似 delete
-	return nil
-}
-
-func (s *skipList) print() {
-	fmt.Println()
-
-	for i := s.level - 1; i >= 0; i-- {
-		current := s.head
-		for current.forward[i] != nil {
-			fmt.Printf("%d ", current.forward[i].key)
-			current = current.forward[i]
-			//代表第i层的下一结点
-		}
-		fmt.Printf("***************** Level %d \n", i+1)
-	}
-}
-
-func main() {
-	list := newSkipList()
-	for i := 0; i < 9; i++ {
-		list.insert(rand.Intn(100))
-	}
-	list.print()
-
-	fmt.Println("\n--------------------------------------")
-
-	list.delete(10)
-	list.print()
-
-	fmt.Println("\n--------------------------------------")
-
-	list.delete(13)
-	list.print()
-
-	fmt.Println("\n--------------------------------------")
-
-}
