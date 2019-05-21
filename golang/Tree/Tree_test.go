fa8c232b633ff3136b0f87bfa63fcd7af2e4e503
yuzp1996 <yuzp1996@qq.com>
2019-05-21 11:01:55 +0800

    add the tree

--- golang/Tree/Tree_test.go
+++ golang/Tree/Tree_test.go
@@ -7,12 +7,26 @@ import (
 )
 
 var _ = Describe("Tree", func() {
+	//var result []int
 	Root := NewTree(0)
 	Root.LeafAdd(1)
+	Root.RightAdd(2)
+	Root.Left.LeafAdd(3)
+	Root.Left.RightAdd(4)
+	Root.Right.LeafAdd(5)
+	Root.Right.RightAdd(6)
 	Context("test the tree", func() {
-
 		It("Insert should be successful", func() {
 			Expect(Root.Left.Value).To(Equal(1))
 		})
+		It("Insert right should be successful", func() {
+			Expect(Root.Right.Value).To(Equal(2))
+		})
 	})
+	//Context("Traversing with fornt", func() {
+	//	result = []int{0,1,3,4,2,5,6}
+	//	It("preorder traversal", func() {
+	//		Expect(Root.Preorder()).To(Equal(result))
+	//	})
+	//})
 })
--- golang/Tree/linkedtree.go
+++ golang/Tree/linkedtree.go
@@ -1,6 +1,9 @@
 package Linkedtree
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 type Leaf struct {
 	Value int
@@ -20,4 +23,24 @@ func (Root *Leaf) LeafAdd(value int) error {
 	} else {
 		return errors.New("Can not add element to this node's left leaf")
 	}
+}
+
+func (Root *Leaf) RightAdd(value int) error {
+	if Root.Right == nil {
+		newleaf := Leaf{value, nil, nil}
+		Root.Right = &newleaf
+		return nil
+	} else {
+		return errors.New("Can not add element to this node's right leaf")
+	}
+}
+
+func(Root *Leaf)Preorder(){
+	if Root == nil{
+		fmt.Print("nil")
+	}else{
+		fmt.Print(Root.Value)
+	}
+	Root.Left.Preorder()
+	Root.Right.Preorder()
 }
