ae1d9488fd1d4b6116981e44ca9fc106c5a3d1c6
yuzp1996 <yuzp1996@qq.com>
2019-07-14 13:57:06 +0800

    heap sort

---
+++ golang/heap/heap.go
@@ -0,0 +1,145 @@
+package heap
+
+type  Heap struct {
+	Data []int
+	Count int
+	Capacity int
+}
+
+func NewHeap(capacity int)*Heap{
+	data  := make([]int, capacity+1, capacity+1)
+	return &Heap{
+		Data:data,
+		// care place 1 the data struct
+		Count:0,
+		Capacity:capacity+1,
+	}
+}
+
+
+func(heap *Heap)BigTopInsert(value int){
+	// care place 2
+	// count should add 1 first  count is lower 1 than capacity
+	heap.Count += 1
+	// if this heap is full
+	if heap.Count >= heap.Capacity{
+		return
+	}
+
+	index := heap.Count
+	heap.Data[index] = value
+
+	for{
+		// now can do the campare
+		if (index)/2>0 && heap.Data[index]>heap.Data[index/2]{
+			heap.Data[index], heap.Data[index/2] = heap.Data[index/2],heap.Data[index]
+			index = index/2
+		}else{
+			break
+		}
+	}
+}
+
+func(heap *Heap)SmallTopInsert(value int){
+	heap.Count += 1
+	if heap.Count >= heap.Capacity{
+		return
+	}
+	heap.Data[heap.Count] = value
+	index := heap.Count
+	for{
+		if index/2 > 0 && heap.Data[index/2] > heap.Data[index]{
+			heap.Data[index], heap.Data[index/2] = heap.Data[index/2], heap.Data[index]
+			index = index/2
+		}else{
+			break
+		}
+	}
+}
+
+
+func(heap *Heap)RemoveBigTop(){
+	if heap.Count <= 1{
+		return
+	}
+	heap.Data[1] = heap.Data[heap.Count]
+	heap.Data[heap.Count] = 0
+	heap.Count -= 1
+
+	index := 1
+	maxposi := 1
+	for{
+		if index*2<=heap.Count&&heap.Data[index*2]>heap.Data[index]{
+			maxposi = index*2
+		}
+		if index*2+1<=heap.Count&& heap.Data[index*2+1]>heap.Data[maxposi]{
+			maxposi = index*2+1
+		}
+		heap.Data[index],heap.Data[maxposi] = heap.Data[maxposi], heap.Data[index]
+		index = maxposi
+		if index*2>heap.Count{
+			break
+		}
+	}
+	return
+}
+
+func (heap *Heap)Sort(){
+	// Build the heap to make sure the top is the biggest element
+	for index := heap.Count/2;index > 0;index--{
+		// index should token out of this function
+		heapindex := index
+		// make the top the biggest value
+		heap.heapify(heapindex)
+		}
+	for i:=1;i<heap.Count;{
+		heap.heapsort()
+	}
+}
+
+func(heap *Heap)heapify(heapindex int){
+	for{
+		maxposi:=0
+		// 这里这个条件很精妙
+		if heapindex*2 <= heap.Count && heap.Data[2*heapindex]>= heap.Data[heapindex]{
+			maxposi = heapindex*2
+		}
+		// I lose the condition 2*index+1 should be lower than count
+		if 2*heapindex+1 <= heap.Count && heap.Data[2*heapindex+1] > heap.Data[maxposi]{
+			maxposi = 2*heapindex+1
+		}
+
+		if maxposi == 0{
+			break
+		}
+		heap.Data[maxposi],heap.Data[heapindex] = heap.Data[heapindex], heap.Data[maxposi]
+		heapindex = maxposi
+	}
+
+}
+
+func (heap *Heap)heapsort() {
+	if heap.Count <= 1{
+		return
+	}
+	heap.Data[1], heap.Data[heap.Count] = heap.Data[heap.Count],heap.Data[1]
+	heap.Count -= 1
+
+	index := 1
+	maxposi := 1
+	for{
+		if index*2<=heap.Count&&heap.Data[index*2]>heap.Data[index]{
+			maxposi = index*2
+		}
+		if index*2+1<=heap.Count&& heap.Data[index*2+1]>heap.Data[maxposi]{
+			maxposi = index*2+1
+		}
+		heap.Data[index],heap.Data[maxposi] = heap.Data[maxposi], heap.Data[index]
+		index = maxposi
+		if index*2>heap.Count{
+			break
+		}
+	}
+	return
+
+}
---
+++ golang/heap/heap_test.go
@@ -0,0 +1,79 @@
+package heap_test
+
+import (
+	"Algorithm-Datastruct/golang/heap"
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Heap", func() {
+	var Heap *heap.Heap
+	BeforeEach(func() {
+		Heap = heap.NewHeap(5)
+	})
+	Context("BigTopheap BigTopInsert should be right ", func() {
+		It("insert the right position", func() {
+			Heap.BigTopInsert(10)
+			Heap.BigTopInsert(5)
+			Heap.BigTopInsert(6)
+			Heap.BigTopInsert(7)
+			Heap.BigTopInsert(12)
+			Heap.BigTopInsert(13)
+
+			Expect(Heap.Data[1]).To(Equal(12))
+			Expect(Heap.Data[2]).To(Equal(10))
+			Expect(Heap.Data[3]).To(Equal(6))
+			Expect(Heap.Data[4]).To(Equal(5))
+			Expect(Heap.Data[5]).To(Equal(7))
+			Expect(Heap.Data[1]).To(Equal(12))
+		})
+	})
+	Context("SmallTopHeap should insert the right value", func() {
+		It("Insert the right value ", func() {
+			Heap.SmallTopInsert(20)
+			Heap.SmallTopInsert(15)
+			Heap.SmallTopInsert(18)
+			Heap.SmallTopInsert(14)
+
+			Expect(Heap.Data[1]).To(Equal(14))
+			Expect(Heap.Data[2]).To(Equal(15))
+			Expect(Heap.Data[3]).To(Equal(18))
+			Expect(Heap.Data[4]).To(Equal(20))
+		})
+	})
+
+	Context("RemoveBigTop should remove the top value", func() {
+		It("remove the top value ", func() {
+			Heap.BigTopInsert(20)
+			Heap.BigTopInsert(15)
+			Heap.BigTopInsert(18)
+			Heap.BigTopInsert(14)
+			Heap.RemoveBigTop()
+			Heap.RemoveBigTop()
+
+
+			Expect(Heap.Data[1]).To(Equal(15))
+			Expect(Heap.Data[2]).To(Equal(14))
+			//Expect(Heap.Data[3]).To(Equal(0))
+			//Expect(Heap.Data[4]).To(Equal(20))
+		})
+	})
+
+	Context("Sort should work ", func() {
+		It("sort should be good", func() {
+
+			Heap.Data[1]=7
+			Heap.Data[2]=3
+			Heap.Data[3]=9
+			Heap.Data[4]=14
+			Heap.Data[5]=16
+			Heap.Count = 5
+
+			Heap.Sort()
+			for i:=1;i<5;i++{
+				Expect(Heap.Data[i]<Heap.Data[i+1]).To(Equal(true))
+			}
+		})
+	})
+
+})
