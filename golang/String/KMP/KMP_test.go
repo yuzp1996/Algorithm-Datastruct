286a18e3ba7b7faaa826dffd27fc59d2c394e277
yuzp1996 <yuzp1996@qq.com>
2019-11-08 10:41:38 +0800

    KMP

--- golang/String/KMP/KMP_test.go
+++ golang/String/KMP/KMP_test.go
@@ -1,12 +1,18 @@
 package KMP_test
 
 import (
+	. "Algorithm-Datastruct/golang/String/KMP"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-
-	. "Algorithm-Datastruct/golang/String/KMP"
 )
 
 var _ = Describe("KMP", func() {
+	It("kmp algorithm", func() {
+		result := MainSkeleTon("abcabcabcd", "abcd")
+		Expect(result).To(Equal(true))
 
+		next := Failurefunction("zpyduzpsyuzpdyuzxpyu")
+		Expect(next).To(Equal([]int{1,3,4}))
+
+	})
 })
--- golang/String/KMP/kmp.go
+++ golang/String/KMP/kmp.go
@@ -1,14 +1,16 @@
 package KMP
 
+import "log"
+
 func MainSkeleTon(main,pattern string)bool{
 	mainlength := len(main)
 	failurearray := Failurefunction(pattern)
 	patternlength := len(pattern)
-	forward := 1
-	for mainindex := 0;mainindex < mainlength - patternlength;mainindex = mainindex+forward{
+	var forward = 1
+	for mainindex := 0; mainindex <= mainlength - patternlength; mainindex += forward{
 		for patternindex:= 0;patternindex < patternlength;patternindex++{
 			if main[mainindex + patternindex] == pattern[patternindex]{
-				if mainindex + patternindex == patternlength{
+				if patternindex == patternlength-1{
 					return true
 				}
 				continue
@@ -16,7 +18,6 @@ func MainSkeleTon(main,pattern string)bool{
 
 			forward = failurearray[patternindex]
 			break
-
 		}
 	}
 	return false
@@ -24,5 +25,38 @@ func MainSkeleTon(main,pattern string)bool{
 
 func Failurefunction(pattern string)[]int{
 	// if the index value is 0  it should be 1  because forward need use it
-	return []int{}
+	log.Printf("len(pattern) is %v", len(pattern))
+	next := []int{len(pattern)}
+	var maxlength int
+	for index := 1; index < len(pattern); index++ {
+		//每次一个数的过滤
+
+
+		//如果前一个值的有最长匹配后缀 && 要匹配的这个值 和 前一个值最长匹配后缀的下一个值不同
+		for (maxlength > 0 && pattern[index]!=pattern[maxlength]){
+			//往回找那种可以匹配的子集
+			maxlength = next[maxlength-1]
+		}
+		//看要匹配的这个值和最长匹配的下一个值是否相同 相同就加一
+		if pattern[index] == pattern[maxlength]{
+			maxlength++
+		}
+		next[index] = maxlength
+
+
+	}
+	return next
 }
+
+
+
+//"ababab"  最长可匹配后缀
+//0 "a"      -1
+//1 "ab"	   -1
+//2 "aba"	   2
+//3 "abab"   2
+//4 "ababa"  2
+//
+//[]int{-1,-1,2,2,2}
+
+
